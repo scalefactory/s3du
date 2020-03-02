a9317ec9f2e1f40ed9a17ebf36506827fbdd0bf5
David O'Rourke <david.orourke@gmail.com>
2020-03-02 21:19:55 +0000

    Forbid unsafe code and deny missing docs

--- src/cloudwatch.rs
+++ src/cloudwatch.rs
@@ -1,4 +1,6 @@
 // s3du: A tool for informing you of the used space in AWS S3.
+#![forbid(unsafe_code)]
+#![deny(missing_docs)]
 use anyhow::{
     anyhow,
     Result,
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-// s3du: A tool for informing you of the used space in AWS S3.
+//! s3du: A tool for informing you of the used space in AWS S3.
+#![forbid(unsafe_code)]
+#![deny(missing_docs)]
 use anyhow::Result;
 use humansize::{
     file_size_opts,
