0aff9d5347c7fccf77e557dadf5f59ed17896b32
David O'Rourke <david.orourke@gmail.com>
2020-10-04 13:23:25 +0100

    cloudwatch::client: Remove a Default::default() from an input struct
    We already initialized all fields

--- src/cloudwatch/client.rs
+++ src/cloudwatch/client.rs
@@ -153,7 +153,6 @@ impl Client {
                 metric_name: Some("BucketSizeBytes".into()),
                 namespace:   Some("AWS/S3".into()),
                 next_token:  next_token,
-                ..Default::default()
             };
 
             // Call the API
